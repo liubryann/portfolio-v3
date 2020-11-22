9d572f497ee477ad083944fc2f695267c61f3378
Bryan Liu <liubryan.developer@gmail.com>
2020-11-22 18:44:03 -0500

    Added new projects, added logo

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,6 +4,8 @@ import { Card } from 'antd';
 import { Divider } from 'antd';
 import uimpactify from '../../img/uimpactify.gif';
 import salesapp from '../../img/sales.gif';
+import dddgif from '../../img/ddd.gif';
+import calculatorgif from '../../img/calculator.gif';
 
 const { Meta } = Card;
 
@@ -26,6 +28,28 @@ const Projects: React.FC = () => {
               <Meta
                 title="Sales Application"
                 description="Mobile shopping platform"
+              />
+            </Card>
+          </a>
+        </Col>
+      </Row>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} xl={12}>
+          <a href="https://github.com/liubryann/ddd">
+            <Card hoverable cover={<img src={dddgif} />}>
+              <Meta
+                title="Due Diligence for Dummies"
+                description="Application that generates the public sentiment of a stock"
+              />
+            </Card>
+          </a>
+        </Col>
+        <Col xs={24} xl={12}>
+          <a href="https://github.com/liubryann/affordable-calculator">
+            <Card hoverable cover={<img src={calculatorgif} />}>
+              <Meta
+                title="Affordable Calculator"
+                description="Android calculator with 30,000 active users (2020)"
               />
             </Card>
           </a>
